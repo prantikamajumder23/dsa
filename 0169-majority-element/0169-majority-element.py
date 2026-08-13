92948d840f107f13a3ba7dbc3b730529da3b74c1
prantikamajumder23 <majumderprantika85@gmail.com>
2026-08-13 23:58:37 +0530

    solved leetcode 169 - majority element  using sorting

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        nums.sort()
+        return nums[len(nums)/2]
+        
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
