e50421541dbcd6ccdf26152c831aa51bf0927d03
prantikamajumder23 <majumderprantika85@gmail.com>
2026-08-19 23:14:17 +0530

    solved rotate matrix by 90 degree on leetcode

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def rotate(self, matrix):
+        n = len(matrix)
+
+        # Transpose
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Reverse each row
+        for row in matrix:
+            row.reverse()
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
