00862e3f7e61f91dfc145ed839d5cb0803f89bb9
prantikamajumder23 <majumderprantika85@gmail.com>
2026-08-28 23:51:49 +0530

    solved subarray sum equals k on leetcode

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        count = 0
+        prefix_sum = 0
+        freq = {0: 1}
+
+        for num in nums:
+            prefix_sum += num
+
+            if prefix_sum - k in freq:
+                count += freq[prefix_sum - k]
+
+            freq[prefix_sum] = freq.get(prefix_sum, 0) + 1
+
+        return count
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
