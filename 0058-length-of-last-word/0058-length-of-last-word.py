b82cc6c2ebf411eac97faa48525a3c7b7140b03f
prantikamajumder23 <majumderprantika85@gmail.com>
2026-09-05 22:44:09 +0530

    solved length of last word in leetcode

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        words = s.split()
+        return len(words[-1])
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
